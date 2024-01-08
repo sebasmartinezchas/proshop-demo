57bc724b9756a03f0cc230d2611a91938811373d
sebasmartinezchas <sebastianmartinezchas@gmail.com>
2024-01-08 20:05:36 -0300

    seeder js mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from 'dotenv'
 dotenv.config();
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 const port =process.env.PORT || 5000;
+connectDB(); // Connect to MondoDB
 const app = express();
 app.get("/", (req, res) => {
   res.send("Api is running");

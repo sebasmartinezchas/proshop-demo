6e0c686a9decedff83e53bc0939fda65ed8ef738
sebasmartinezchas <sebastianmartinezchas@gmail.com>
2024-01-10 20:07:48 -0300

    redux store part1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,24 +6,27 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store.js";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import App from "./App.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import ProductScreen from "./screens/ProductScreen.jsx";
 
-const router=createBrowserRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App/>}>
-      <Route index={true} path="/" element={<HomeScreen/>}/>
-      <Route path="/product/:id" element={<ProductScreen/>}/>
-
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
-)
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
- <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );

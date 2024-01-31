4794349a386b5461913ce176f2ee3e6ffe2d6629
sebasmartinezchas <sebastianmartinezchas@gmail.com>
2024-01-31 17:43:04 -0300

    paypal implemented

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -45,7 +45,7 @@ const router = createBrowserRouter(
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PayPalScriptProvider deferLoading={true}>
+      <PayPalScriptProvider deferLoading={false}>
         <RouterProvider router={router} />
       </PayPalScriptProvider>
     </Provider>
